db5f053462d6e53399abdb53f393d2378c298c58
Kiruba N <142664078+Kiruba0052@users.noreply.github.com>
2024-08-22 20:59:56 +0530

    Add files via upload

---
+++ Quiz-Application-Java/src/quizapp/Main.java
@@ -0,0 +1,18 @@
+
+package quizapp;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import quizapp.ui.Login;
+import repositories.JsonFileRepository;
+
+public class Main {
+
+      public static void main(String[] args) {
+        
+        new Login().setVisible(true);
+        
+    }
+
+}
